06e80ef4f8b9938b3a82eec93cdcd09117baebcc
zhanghua <zhanghua10240@gmail.com>
2020-03-29 00:01:39 +0800

    整合 Swagger

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# mall
-主流技术栈开发一个商城系统
+# 风暴商城
+使用主流技术栈开发一个商城系统。
+
+[TOC]
+
+
+
+## 项目搭建流程：
+### 1，SpringBoot + MyBatis搭建基本骨架
+
+| 编号 |    技术栈    |    详情    |
+| :--: | :----------: | :--------: |
+|  00  |    数据库    |   MySql    |
+|  01  | 数据库连接池 |   Druid    |
+|  02  |   ORM 框架   |  MyBatis   |
+|  03  |   分页插件   | PageHelper |
+
+- 新建接口通用返回类；
+
+- 定义业务状态码；
+
+- 统一处理异常；
+
+- 完成分页查询接口;
+
+  ```java
+      @GetMapping("/list")
+      public ApiResponse listBrand(@RequestParam(value = "pageNum",defaultValue = "1")  Integer pageNum, @RequestParam(value = "pageSize",defaultValue = "5") Integer pageSize){
+          List<PmsBrand> list = brandService.listBrand(pageNum,pageSize);
+          return new ApiResponse(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(),list);
+      }
+  ```
+
+
+
+### 2，SpringBoot + MyBatis搭建基本骨架
+
+Swagger-UI是HTML, Javascript, CSS的一个集合，可以动态地根据注解生成在线API文档；
+
+在很久很久之前，我开发接口是这样的，先写接口文档，一个一个填写参数，然后用 postman 测试，接口调通了之后告诉前端，可以进行接口联调了...
+
+现在用 Swagger 你就不用写接口文档了，而且可以直接在页面上进行接口测试，这种感觉怎么说呢，别问，问就是温暖。
+
+使用流程：
+
+- 添加swagger 依赖；
+- 配置 swagger，指定使用什么规则生成 API 文档；
+- 在需要生成接口文档接口上添加 Swagger注解；
+- 重新运行项目，打开http://localhost:8080/swagger-ui.html；
+
+### 3，整合Redis实现缓存功能
+
+
+
+
+
--- src/main/java/com/storm/mall/controller/PmsBrandController.java
+++ src/main/java/com/storm/mall/controller/PmsBrandController.java
@@ -4,6 +4,8 @@ import com.storm.mall.common.api.ApiResponse;
 import com.storm.mall.common.api.ResultCode;
 import com.storm.mall.entity.PmsBrand;
 import com.storm.mall.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,7 @@ import java.util.List;
  * @CreateTime 2020-03-28 20:41
  */
 
+@Api(tags = "PmsBrandController",description = "商品品牌管理")
 @RestController
 @RequestMapping("/brand")
 public class PmsBrandController {
@@ -28,6 +31,7 @@ public class PmsBrandController {
     private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
 
 
+    @ApiOperation("获取所有品牌列表")
     @GetMapping("/listAll")
     public ApiResponse getBrandList(){
 
@@ -37,15 +41,15 @@ public class PmsBrandController {
     }
 
 
-
+    @ApiOperation("分页获取品牌列表")
     @GetMapping("/list")
-    public ApiResponse listBrand(@RequestParam(value = "pageNum",defaultValue = "1")  Integer pageNum,
-                                 @RequestParam(value = "pageSize",defaultValue = "5") Integer pageSize){
+    public ApiResponse listBrand(@RequestParam(value = "pageNum",defaultValue = "1")  Integer pageNum, @RequestParam(value = "pageSize",defaultValue = "5") Integer pageSize){
         List<PmsBrand> list = brandService.listBrand(pageNum,pageSize);
         return new ApiResponse(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(),list);
-
     }
 
+
+    @ApiOperation("根据 id 获取品牌详情")
     @GetMapping("/{id}")
     public ApiResponse getBrandById(@PathVariable long id){
         PmsBrand brand = brandService.getBrand(id);
